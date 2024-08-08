87342768984d3460bb4b9b17b36f5baca605f373
Takanobu Suzuki <nobublack555@gmail.com>
2024-08-08 14:22:07 -0700

    fix viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import { Header } from '@/components/common/Header'
 import './globals.css'
 import { Flex } from '@/components/ui/Flex'
@@ -6,6 +6,10 @@ import { Flex } from '@/components/ui/Flex'
 export const metadata: Metadata = {
   title: 'Football',
   description: 'You can find the football matches here',
+}
+
+export const viewport: Viewport = {
+  width: '1100',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
